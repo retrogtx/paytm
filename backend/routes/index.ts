ea156865f133fe194b845a9a2d2be52e278e88c0
Amrit Rai <iamamrit27@gmail.com>
2024-07-06 12:16:11 +0530

    add schema

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,4 +1,6 @@
+import express from "express";
 const userRouter = require("./user");
+const router = express.Router();
 
 router.use("/user", userRouter)
 
--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,5 +1,29 @@
-const express = require('express');
+import express from "express";
+import { Router } from "express";
+import { User } from "../db";
 
-const router = express.Router();
+const zod = require("zod");
+const router = Router();
+
+const signupSchema = zod.object({
+    username: zod.string(),
+    password: zod.string(),
+    firstName: zod.string()
+})
+
+router.post("signup", (req, res) => {
+    const body = req.body;
+    const { success } = signupSchema.safeParse(req.body);
+
+    if (!success) {
+        return res.json ({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const user = User.findOne({
+        username: body.username
+    })
+})
 
 module.exports = router;
