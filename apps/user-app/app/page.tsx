505ca17673dbe9252330085a821a1cac1bd7c5d0
Amrit Rai <iamamrit27@gmail.com>
2024-08-31 10:58:11 +0530

    chore: Update project configuration files

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'; 
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
 
-export default function Home() {
+export default function Page(): JSX.Element {
+  const session = useSession();
   return (
-    <div className="flex items-center justify-center h-screen">
-      hi there
-    </div>
+   <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+   </div>
   );
 }
