ed14baa8a925bee8b81b59ab17165cb311daa029
Turbobot <turbobot@vercel.com>
2024-08-31 08:32:10 +0530

    feat(create-turbo): apply official-starter transform

--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SendCard } from "../../../components/SendCard";
-import { History } from "../../../components/History";
-export default function() {
-    return <div className="w-full">
-        <History/>
-    </div>
-}
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,14 +4,17 @@ import { ReactNode } from "react";
 
 interface ButtonProps {
   children: ReactNode;
-  onClick: () => void;
+  className?: string;
+  appName: string;
 }
 
-export const Button = ({ onClick, children }: ButtonProps) => {
+export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
-    <button onClick={onClick} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+    <button
+      className={className}
+      onClick={() => alert(`Hello from your ${appName} app!`)}
+    >
       {children}
     </button>
-
   );
 };
