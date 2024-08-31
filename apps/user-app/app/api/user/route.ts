38cab42c1850f6604b3116add8be58e5939aa4de
Amrit Rai <iamamrit27@gmail.com>
2024-08-31 11:14:53 +0530

    Add null check for session user in user route for typescript to calm down

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -4,10 +4,10 @@ import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
     const session = await getServerSession(authOptions);
-    if (session.user) {
+    if (session?.user) {
         return NextResponse.json({
             user: session.user
-        })
+        });
     }
     return NextResponse.json({
         message: "You are not logged in"
